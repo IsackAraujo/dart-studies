b48788d25af650520a6471333e688e59ab680005
Isack <isackaraujoc@gmail.com>
2024-04-21 15:14:18 -0300

    Teste

--- Ex5/VariablesAndLists.dart
+++ Ex5/VariablesAndLists.dart
@@ -35,10 +35,10 @@ print('${both.key} = ${both.value}');
 
 final int code = 34;
 
-  // code = 3; Impossible to run, because code is a Const variable that       are already defined
+  // code = 3; Impossible to run, because code is a Const variable that are already defined
 
 const String birth = '03-04-2004';
 
-  // birth = '20-03-1982'; Impossible to run, because birth is a Final        variable that are already defined
+  // birth = '20-03-1982'; Impossible to run, because birth is a Final are variable that are already defined
 
 }
