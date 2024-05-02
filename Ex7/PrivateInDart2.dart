39dd1b2f44164b81f4f99cf33f41e9015589399c
IsackAraujo <isackaraujoc@gmail.com>
2024-05-02 00:41:33 -0300

    Private Parameters in Dart2

---
+++ Ex7/PrivateInDart2.dart
@@ -0,0 +1,11 @@
+import "./PrivateInDart.dart";
+
+main () {
+
+  var person1 = new Person();
+
+ person1.name = "Cleyton";
+  // person1._id = 10; // <- Not an possible action because id its a private parameter
+
+  print("I can just print the name!! Name: ${person1.name} :(");
+}
