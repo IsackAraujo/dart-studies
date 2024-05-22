085f61af9908b78c44a81fef2a854f667b50a5ba
IsackAraujo <isackaraujoc@gmail.com>
2024-05-21 23:35:28 -0300

    Streams#1

--- Ex8/Stream.dart
+++ Ex8/Stream.dart
@@ -1,3 +1,16 @@
 /// Gotta learn how to use Stream in dart
 /// For Works porpuses
 /// So lets practice!!!
+
+void main() {
+  print("Oi");
+
+  final streamEstudos = Stream<int>.periodic(
+    Duration(seconds: 5),
+    (index) => index + 1,
+  ).take(10);
+
+  streamEstudos.listen((valor) {
+    print(valor);
+  });
+}
